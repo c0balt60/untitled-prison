f48a556ab5b202b6ccad1f33a8bf050819b29fad
c0balt60 <elmt.c0balt60@gmail.com>
2025-10-28 19:25:41 -0400

    refactor(story): update storybook

--- src/client/ui/stories/home.story.tsx
+++ src/client/ui/stories/home.story.tsx
@@ -2,7 +2,7 @@ import React from "@rbxts/react";
 import ReactRoblox from "@rbxts/react-roblox";
 import { Boolean, type InferProps } from "@rbxts/ui-labs";
 
-import { App } from "../app/app";
+import { Home } from "../app/menu/home";
 
 const controls = {
 	Visible: Boolean(true),
@@ -12,7 +12,9 @@ const app = {
 	controls,
 	react: React,
 	reactRoblox: ReactRoblox,
-	story: (_props: InferProps<typeof controls>): React.ReactNode => App(),
+	story: (props: InferProps<typeof controls>): React.ReactNode => {
+		return <Home show={props.controls.Visible} />;
+	},
 };
 
 export = app;
